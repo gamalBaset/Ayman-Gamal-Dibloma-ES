8d8a4a4e0bde040e57558307d75ec42fc204b0c9
gamalBaset <109480949+gamalBaset@users.noreply.github.com>
2022-07-17 20:46:53 +0200

    first main
    i added first object

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: ayman
+ */
+
+#include "stdio.h"
+void main ()
+{
+	printf ("hello \n");
+	int x = 0 ;
+	for ( x ;x<3  ;x++ ){
+		printf ("ayman gamal \n");
+	}
+	printf ("bye ");
+}
